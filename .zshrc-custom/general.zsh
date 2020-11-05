0aeaea95d5e99efb9e536f40383484c2406278ae
qxxxb <46016483+qxxxb@users.noreply.github.com>
2020-11-04 22:59:02 -0500

    Add zshrc stuff

--- .zshrc-custom/general.zsh
+++ .zshrc-custom/general.zsh
@@ -198,3 +198,5 @@ if [ -f /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh ]; then
     # change suggestion color
     ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=#999'
 fi
+
+export FZF_DEFAULT_OPTS="--reverse --border"
